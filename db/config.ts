ade4b5e0d24c0fa5dc8eddc3cd4ca1243e424b2b
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2024-04-14 14:11:43 +0200

    initial db test

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,35 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable } from "astro:db";
+
+const Locales = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    name: column.text(),
+  },
+});
+
+const I18nKeys = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    key: column.text(),
+  },
+});
+
+const Translations = defineTable({
+  columns: {
+    localeId: column.number({ references: () => Locales.columns.id }),
+    i18nKeyId: column.number({ references: () => I18nKeys.columns.id }),
+    value: column.text(),
+  },
+});
+
+const Navigation = defineTable({
+  columns: {
+    i18nKeyId: column.number({ references: () => I18nKeys.columns.id }),
+    href: column.text(),
+  },
+});
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: { Locales, I18nKeys, Translations, Navigation },
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,11 @@
-import { db } from 'astro:db';
+import { db, I18nKeys, Locales, Navigation, Translations } from "astro:db";
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+  await db.insert(Locales).values([{ id: 1, name: "English" }]);
+  await db.insert(I18nKeys).values([{ id: 1, key: "Home Navigation" }]);
+  await db
+    .insert(Translations)
+    .values([{ localeId: 1, i18nKeyId: 1, value: "Home" }]);
+  await db.insert(Navigation).values([{ i18nKeyId: 1, href: "/" }]);
 }
